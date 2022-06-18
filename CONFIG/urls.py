23fdf2efa5ca19c8c4a68c72a4c3acdaaddf7eb7
Didisimmons <onyema.onyejekwe@gmail.com>
2022-06-18 03:01:22 +0100

    add home page content and images

--- CONFIG/urls.py
+++ CONFIG/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('donate/', include('charity.urls')),
+    path('', include('home.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
