28481f8a89b9e2633c092d051cb5b44e83d45c31
Daniel Callaghan <dan_cal89@hotmail.com>
2022-06-18 11:30:49 +0000

    update media url context processor

--- event/models.py
+++ event/models.py
@@ -1,18 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+# real time chat
+# contacting the organizer - inbox for the event / group chat for volunteers  - users have an inbox
+# comments
+
+
+
 class Event(models.Model):
     """Model for events"""
 
     name = models.CharField(max_length=50)
+    featured_image = models.ImageField(default='default.jpg', upload_to='event_featured_images')
     description = models.TextField()
-    slug = models.SlugField(max_length=255)
+    slug = models.SlugField(max_length=255) # make unique
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    location = models.CharField(max_length=50)
+    location = models.CharField(max_length=50) # look in to cities
     organizer = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateField()
     time = models.TimeField()
+    volunteer_list = models.ManyToManyField(User, blank=True, related_name='volunteers')
+
 
     def __str__(self):
         return f'{self.organizer.username} event {self.name}'
@@ -30,3 +39,12 @@ class Donation(models.Model):
 
     def __str__(self):
         return f'Donation for {self.event.name}'
+
+# class Volunteer(models.Model):
+#     event = models.ForeignKey(Event, on_delete=models.CASCADE)
+# pledge then -> the site asks for money 
+# pledge - then pay
+
+# donations build up
+
+# target amount - 
