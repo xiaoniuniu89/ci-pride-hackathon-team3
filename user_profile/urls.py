0fd09fffa0e134de90ba3aec5fdc3e9a721fe3c4
Felix-Voyle <felix.voyle@icloud.com>
2022-06-19 10:06:52 +0000

    basic user profile page layout added url for custom change password

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from .views import (
     profile
 )
 
 urlpatterns = [
     path('', profile, name='profile'),
+    path('changepassword/', auth_views.PasswordChangeView.as_view(template_name='user_profile/change-password.html')),
 ]
