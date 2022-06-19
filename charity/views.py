6d65742968da62b0468639ca6f8e24dada57e4f5
Didisimmons <onyema.onyejekwe@gmail.com>
2022-06-19 02:49:41 +0100

    add stripe payment in views

--- charity/views.py
+++ charity/views.py
@@ -9,6 +9,8 @@ from .models import Donations
 from .models import Event
 from .forms import DonationForm
 
+import stripe
+
 def donate(request):
     """ A view to return the donation page """
     return render(request, 'charity/charity.html')
@@ -16,11 +18,15 @@ def donate(request):
 def makedonations(request):
     """ A view to return the donation page 
     and handle all donations"""
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
 
     if request.method == 'POST':
         form = DonationForm(request.POST)
         if form.is_valid():
             donor = form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            donor.stripe_pid = pid
             donor.user = request.user
             donor.save()
             messages.info(request, 'Successfully made a donation')
@@ -31,8 +37,15 @@ def makedonations(request):
     else:
         form = DonationForm()
 
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+                    amount=request.POST.get('amount'),
+                    currency=settings.STRIPE_CURRENCY,
+                )
     template = 'charity/donate.html'
     context = {
         'form': form,
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
     return render(request, template, context)
