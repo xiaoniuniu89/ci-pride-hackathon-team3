05bfccfec8a460aa14485e8ff49b274abb2c94c5
Didisimmons <onyema.onyejekwe@gmail.com>
2022-06-19 04:42:00 +0100

    add success logic to sucess.html and adjust charity model

--- charity/models.py
+++ charity/models.py
@@ -4,22 +4,22 @@ from event.models import Event
 
 
 class Donations(models.Model):
-    ConsentWorkshop = 'CW'
-    OutrightInternational = 'OI'
-    GlobalGiving = 'GG'
-    MicroRianbow = 'MR'
+    ConsentWorkshop = 'Consent Workshop'
+    OutrightInternational = 'Outright International'
+    GlobalGiving = 'GlobalGiving'
+    MicroRainbow = 'MicroRainbow'
     SELECT = [
         (ConsentWorkshop, 'Consent Workshop'),
         (OutrightInternational, 'Outright International'),
         (GlobalGiving, 'Global Giving'),
-        (MicroRianbow, 'Micro Rianbow'),
+        (MicroRainbow, 'Micro Rainbow'),
     ]
 
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     email = models.EmailField(max_length=254, null=False, blank=False)
     event_name = models.ForeignKey(Event, on_delete=models.CASCADE)
-    charities = models.CharField(choices=SELECT, max_length=2)
+    charities = models.CharField(choices=SELECT, max_length=22)
     amount = models.DecimalField(max_digits=6, decimal_places=0,
                                         null=False, default=0)
     
